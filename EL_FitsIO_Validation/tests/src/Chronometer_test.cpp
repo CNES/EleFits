6dd2d54f7d61326295c60928132f88a6383988dd
Antoine Basset <antoine.basset@cnes.fr>
2021-06-13 16:10:21 +0200

    Review Chronometer test

--- EL_FitsIO_Validation/tests/src/Chronometer_test.cpp
+++ EL_FitsIO_Validation/tests/src/Chronometer_test.cpp
@@ -17,23 +17,22 @@
  *
  */
 
+#include "EL_FitsIO_Validation/Chronometer.h"
+
 #include <boost/test/unit_test.hpp>
 #include <cstdlib>
 #include <thread>
-
-#include "EL_FitsIO_Validation/Chronometer.h"
 
 using namespace Euclid::FitsIO;
 
 struct ChronoFixture : public Test::Chronometer<std::chrono::milliseconds> {
   ChronoFixture(std::chrono::milliseconds chronoOffset = std::chrono::milliseconds { std::rand() }) :
-      Test::Chronometer<std::chrono::milliseconds>(chronoOffset),
-      offset(chronoOffset) {
-  }
-  void wait(std::int64_t duration = 2) {
-    std::this_thread::sleep_for(Unit(duration));
+      Test::Chronometer<std::chrono::milliseconds>(chronoOffset), offset(chronoOffset) {}
+  void wait(std::int64_t ms = defaultWait) {
+    std::this_thread::sleep_for(Unit(ms));
   }
   Unit offset;
+  static constexpr std::int64_t defaultWait = 10;
 };
 
 //-----------------------------------------------------------------------------
@@ -43,47 +42,48 @@ BOOST_FIXTURE_TEST_SUITE(Chronometer_test, ChronoFixture)
 //-----------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_CASE(init_test) {
-  BOOST_CHECK_EQUAL(elapsed().count(), offset.count());
-  BOOST_CHECK(not isRunning());
-  BOOST_CHECK_EQUAL(count(), 0);
+  BOOST_TEST(elapsed().count() == offset.count());
+  BOOST_TEST(not isRunning());
+  BOOST_TEST(count() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(one_inc_test) {
   start();
-  BOOST_CHECK(isRunning());
+  BOOST_TEST(isRunning());
   wait();
   stop();
-  BOOST_CHECK(not isRunning());
-  BOOST_CHECK_GT(elapsed().count(), offset.count());
-  BOOST_CHECK_EQUAL(count(), 1);
+  BOOST_TEST(not isRunning());
+  BOOST_TEST(elapsed().count() >= offset.count());
+  BOOST_TEST(count() == 1);
   const auto inc = last().count();
-  BOOST_CHECK_EQUAL(elapsed().count(), offset.count() + inc);
-  BOOST_CHECK_EQUAL(mean(), inc);
-  BOOST_CHECK_EQUAL(stdev(), 0.);
-  BOOST_CHECK_EQUAL(min(), inc);
-  BOOST_CHECK_EQUAL(max(), inc);
+  BOOST_TEST(inc >= defaultWait);
+  BOOST_TEST(elapsed().count() == offset.count() + inc);
+  BOOST_TEST(mean() == inc);
+  BOOST_TEST(stdev() == 0.); // Exactly 0.
+  BOOST_TEST(min() == inc);
+  BOOST_TEST(max() == inc);
 }
 
 BOOST_AUTO_TEST_CASE(two_incs_test) {
   start();
-  wait();
+  wait(); // Wait
   stop();
   start();
-  BOOST_CHECK(isRunning());
-  wait();
-  wait(); // Wait more
+  BOOST_TEST(isRunning());
+  wait(defaultWait * 10); // Wait more
   stop();
-  BOOST_CHECK(not isRunning());
-  BOOST_CHECK_GT(elapsed().count(), offset.count());
-  BOOST_CHECK_EQUAL(count(), 2);
-  const auto first = increments()[0];
-  const auto second = increments()[1];
-  BOOST_CHECK_EQUAL(elapsed().count(), offset.count() + first + second);
-  BOOST_CHECK_GE(mean(), first);
-  BOOST_CHECK_LE(mean(), second);
-  BOOST_CHECK_GT(stdev(), 0.);
-  BOOST_CHECK_EQUAL(min(), first);
-  BOOST_CHECK_EQUAL(max(), second);
+  BOOST_TEST(not isRunning());
+  BOOST_TEST(elapsed().count() > offset.count());
+  BOOST_TEST(count() == 2);
+  const auto fast = increments()[0];
+  const auto slow = increments()[1];
+  BOOST_TEST(fast < slow); // FIXME Not that sure!
+  BOOST_TEST(elapsed().count() == offset.count() + fast + slow);
+  BOOST_TEST(mean() >= fast);
+  BOOST_TEST(mean() <= slow);
+  BOOST_TEST(stdev() > 0.);
+  BOOST_TEST(min() == fast);
+  BOOST_TEST(max() == slow);
 }
 
 //-----------------------------------------------------------------------------
