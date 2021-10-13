bfe58240ec39d94cc124f03e770619aef30b7d9d
Antoine Basset <antoine.basset@cnes.fr>
2021-10-13 15:52:48 +0200

    Test PositionIterator

--- EL_FitsData/tests/src/PositionIterator_test.cpp
+++ EL_FitsData/tests/src/PositionIterator_test.cpp
@@ -18,8 +18,11 @@
  */
 
 #include "EL_FitsData/PositionIterator.h"
+#include "EL_FitsData/Raster.h"
 
 #include <boost/test/unit_test.hpp>
+
+using namespace Euclid::FitsIO;
 
 //-----------------------------------------------------------------------------
 
@@ -27,9 +30,34 @@ BOOST_AUTO_TEST_SUITE(PositionIterator_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE(example_test) {
+BOOST_AUTO_TEST_CASE(shape_is_screened_in_order_test) {
+  Position<5> shape { 2, 3, 4, 5, 6 };
+  VecRaster<long, 5> raster(shape);
+  for (long i = 0; i < raster.size(); ++i) {
+    raster.vector()[i] = i; // FIXME implement Raster::operator[](long)
+  }
+  long i = 0;
+  for (const auto& p : raster.domain()) {
+    BOOST_TEST(raster.index(p) == i);
+    ++i;
+  }
+}
 
-  BOOST_FAIL("!!!! Please implement your tests !!!!");
+BOOST_AUTO_TEST_CASE(region_is_screened_in_order_test) {
+  Position<4> shape { 3, 4, 5, 6 };
+  VecRaster<long, 4> raster(shape);
+  Region<4> region { Position<4>::zero() + 1, shape - 2 };
+  for (long i = 0; i < raster.size(); ++i) {
+    raster.vector()[i] = i; // FIXME implement Raster::operator[](long)
+  }
+  long current = 0;
+  long count = 0;
+  for (const auto& p : region) {
+    BOOST_TEST(raster.index(p) > current);
+    current = raster.index(p);
+    ++count;
+  }
+  BOOST_TEST(count == region.size());
 }
 
 //-----------------------------------------------------------------------------
