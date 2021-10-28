9c426e992b32336af96255dd4b31bc649d2c0381
Antoine Basset <antoine.basset@cnes.fr>
2021-10-28 11:27:29 +0200

    Fix documentation link

--- 4.0.0/search/pages_2.js
+++ 4.0.0/search/pages_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['installation_20and_20configuration_20guide_4221',['Installation and configuration guide',['../install_guide.html',1,'']]]
+  ['installation_20and_20configuration_20guide_4221',['Installation and configuration guide',['../md_doc_INSTALL.html',1,'']]]
 ];
