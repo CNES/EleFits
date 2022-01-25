35186518f9ed74e3dc62eaf077b1a325ae9e6ebe
Antoine Basset <antoine.basset@cnes.fr>
2022-01-25 21:11:07 +0100

    Propose bintable-based AstroObjs

--- EleFitsExamples/src/program/EleFitsGenerateAstroObj.cpp
+++ EleFitsExamples/src/program/EleFitsGenerateAstroObj.cpp
@@ -28,56 +28,85 @@
 
 using boost::program_options::value;
 
-using namespace Euclid;
-using namespace Fits;
+using namespace Euclid::Fits;
 
-void writeMeta(MefFile& f, int objIndex) {
-  std::string extname = std::to_string(objIndex) + "_META";
-  const auto& ext = f.initImageExt<unsigned char, 1>(extname, {0});
-  ext.header().writeSeq(
-      Record<int>("DITH_NUM", 0), // TODO
-      Record<int>("SOURC_ID", objIndex),
-      Record<float>("RA_OBJ", float(2 * objIndex)),
-      Record<float>("DEC_OBJ", float(3 * objIndex)));
-}
+struct AstroObjInfo {
+  long combSize;
+  long dithCount;
+  long dith1Size;
+  Position<2> dith2Shape;
+};
 
-void writeCombinedSignal(MefFile& f, int objIndex, int bins) {
-  auto wminData = Test::generateRandomVector<float>(bins);
-  auto signalData = Test::generateRandomVector<float>(bins);
-  auto qualityData = Test::generateRandomVector<char>(bins);
-  auto varData = Test::generateRandomVector<float>(bins);
-  const long repeatCount = 1;
-  PtrColumn<float> wminCol({"WMIN", "nm", repeatCount}, bins, wminData.data());
-  PtrColumn<float> signalCol({"SIGNAL", "erg", repeatCount}, bins, signalData.data());
-  PtrColumn<char> qualityCol({"QUALITY", "", repeatCount}, bins, qualityData.data());
-  PtrColumn<float> varCol({"VAR", "erg^2", repeatCount}, bins, varData.data());
-  std::string extname = std::to_string(objIndex) + "_COMBINED1D_SIGNAL";
-  const auto& ext = f.assignBintableExt(extname, wminCol, signalCol);
-  ext.columns().init(qualityCol.info());
-  ext.columns().init(varCol.info());
-  ext.columns().writeSeq(qualityCol, varCol);
-  ext.header().writeSeq(
-      Record<float>("WMIN", 0.F),
-      Record<float>("BINWIDTH", 1.F),
-      Record<int>("BINCOUNT", bins),
-      Record<float>("EXPTIME", 3600.F));
-}
+void writeExt(MefFile& f, const std::string& name, const AstroObjInfo& info, long rowCount) {
 
-void writeCombinedCov(MefFile& f, int objIndex, int bins) {
-  Test::RandomRaster<float, 2> cov_raster({bins, bins});
-  std::string extname = std::to_string(objIndex) + "_COMBINED1D_COV";
-  const auto& ext = f.assignImageExt(extname, cov_raster);
-  ext.header().writeSeq(Record<int>("COV_SIDE", bins), Record<std::string>("CODEC", "IDENTITY"));
-}
+  /* Metadata */
 
-void writeCombined(MefFile& f, int objIndex, int bins) {
-  writeCombinedSignal(f, objIndex, bins);
-  writeCombinedCov(f, objIndex, bins);
-}
+  ColumnInfo<std::int64_t> objIdInfo("OBJECT_ID");
+  ColumnInfo<std::complex<float>> objRadecInfo("RA_DEC", "deg");
+  ColumnInfo<float> expTimeInfo("EXPTIME", "s");
 
-void writeAstroObj(MefFile& f, int objIndex, int bins) {
-  writeMeta(f, objIndex);
-  writeCombined(f, objIndex, bins);
+  /* Combined */
+
+  const auto combSize = info.combSize;
+
+  ColumnInfo<float> combSigInfo("COMBINED1D_SIGNAL", "erg", combSize);
+  ColumnInfo<std::int32_t> combQualInfo("COMBINED1D_QUALITY", "", combSize);
+  ColumnInfo<float> combVarInfo("COMBINED1D_VAR", "erg^2", combSize);
+
+  /* Dithers */
+
+  const auto dithCount = info.dithCount;
+  const auto dith1Size = info.dith1Size;
+  const auto dith2Width = info.dith2Shape[0];
+  const auto dith2Height = info.dith2Shape[1];
+
+  ColumnInfo<std::int64_t> ptgIdInfo("PTGID", "", dithCount);
+
+  ColumnInfo<float, 2> dith1SigInfo("DITH1D_SIGNAL", "erg", {dith1Size, dithCount});
+  ColumnInfo<std::int32_t, 2> dith1QualInfo("DITH1D_QUALITY", "", {dith1Size, dithCount});
+  ColumnInfo<float, 2> dith1VarInfo("DITH1D_VAR", "erg^2", {dith1Size, dithCount});
+
+  ColumnInfo<float, 3> dith2SigInfo("DITH2D_SIGNAL", "erg", {dith2Width, dith2Height, dithCount});
+  ColumnInfo<std::int32_t, 3> dith2QualInfo("DITH2D_QUALITY", "", {dith2Width, dith2Height, dithCount});
+  ColumnInfo<float, 3> dith2VarInfo("DITH2D_VAR", "erg^2", {dith2Width, dith2Height, dithCount});
+
+  /* Random data */
+
+  const auto objIdData = Test::generateRandomVector<std::int64_t>(rowCount);
+  const auto objRadecData = Test::generateRandomVector<std::complex<float>>(rowCount);
+  const auto expTimeData = Test::generateRandomVector<float>(rowCount);
+
+  const auto combSigData = Test::generateRandomVector<float>(combSize * rowCount);
+  const auto combQualData = Test::generateRandomVector<std::int32_t>(combSize * rowCount);
+  const auto combVarData = Test::generateRandomVector<float>(combSize * rowCount);
+
+  const auto ptgIdData = Test::generateRandomVector<std::int64_t>(dithCount * rowCount);
+
+  const auto dith1SigData = Test::generateRandomVector<float>(dith1Size * dithCount * rowCount);
+  const auto dith1QualData = Test::generateRandomVector<std::int32_t>(dith1Size * dithCount * rowCount);
+  const auto dith1VarData = Test::generateRandomVector<float>(dith1Size * dithCount * rowCount);
+
+  const auto dith2SigData = Test::generateRandomVector<float>(dith2Width * dith2Height * dithCount * rowCount);
+  const auto dith2QualData = Test::generateRandomVector<std::int32_t>(dith2Width * dith2Height * dithCount * rowCount);
+  const auto dith2VarData = Test::generateRandomVector<float>(dith2Width * dith2Height * dithCount * rowCount);
+
+  /* Write! */
+
+  f.assignBintableExt(
+      name,
+      makeColumn(objIdInfo, std::move(objIdData)),
+      makeColumn(objRadecInfo, std::move(objRadecData)),
+      makeColumn(expTimeInfo, std::move(expTimeData)),
+      makeColumn(combSigInfo, std::move(combSigData)),
+      makeColumn(combQualInfo, std::move(combQualData)),
+      makeColumn(combVarInfo, std::move(combVarData)),
+      makeColumn(ptgIdInfo, std::move(ptgIdData)),
+      makeColumn(dith1SigInfo, std::move(dith1SigData)),
+      makeColumn(dith1QualInfo, std::move(dith1QualData)),
+      makeColumn(dith1VarInfo, std::move(dith1VarData)),
+      makeColumn(dith2SigInfo, std::move(dith2SigData)),
+      makeColumn(dith2QualInfo, std::move(dith2QualData)),
+      makeColumn(dith2VarInfo, std::move(dith2VarData)));
 }
 
 class EleFitsGenerateAstroObj : public Elements::Program {
@@ -86,8 +115,11 @@ public:
   std::pair<OptionsDescription, PositionalOptionsDescription> defineProgramArguments() override {
     Euclid::Fits::ProgramOptions options("Generate a random AstroObj file, as specified in the SpectrumLib.");
     options.positional("output", value<std::string>()->default_value("/tmp/astroobj.fits"), "Output file");
-    options.named("nobj", value<int>()->default_value(1), "AstroObj count");
-    options.named("nbin", value<int>()->default_value(1000), "Wavelength bin count");
+    options.named("nobj", value<long>()->default_value(1), "AstroObj count per HDU");
+    options.named("nhdu", value<long>()->default_value(1), "HDU count");
+    options.named("nbin", value<long>()->default_value(1000), "Wavelength bin count");
+    options.named("ndith", value<long>()->default_value(4), "Dither count per AstroObj");
+    options.named("height", value<long>()->default_value(15), "Dither 2D height");
     return options.asPair();
   }
 
@@ -96,8 +128,13 @@ public:
     Elements::Logging logger = Elements::Logging::getLogger("EleFitsGenerateAstroObj");
 
     std::string filename = args["output"].as<std::string>();
-    int nobj = args["nobj"].as<int>();
-    int nbin = args["nbin"].as<int>();
+    const auto nobj = args["nobj"].as<long>();
+    const auto nhdu = args["nhdu"].as<long>();
+    const auto nbin = args["nbin"].as<long>();
+    const auto ndith = args["ndith"].as<long>();
+    const auto height = args["height"].as<long>();
+
+    AstroObjInfo info {nbin, ndith, nbin, {nbin, height}};
 
     logger.info() << "Creating Fits file: " << filename;
     MefFile f(filename, FileMode::Overwrite);
@@ -105,10 +142,12 @@ public:
     const auto& primary = f.primary();
     primary.header().write("N_OBJ", nobj);
 
-    for (int i = 0; i < nobj; ++i) {
-      logger.info() << "Writing AstroObj " << i;
-      writeAstroObj(f, i, nbin);
+    for (long i = 0; i < nhdu; ++i) {
+      logger.info() << "Writing HDU " << i + 1;
+      writeExt(f, std::to_string(i + 1), info, nobj);
     }
+
+    logger.info() << "Done.";
     return Elements::ExitCode::OK;
   }
 };
