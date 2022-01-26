f729842e4d7a841688403bda2a3494e73b975328
Antoine Basset <antoine.basset@cnes.fr>
2022-01-26 12:06:35 +0100

    Make quality columns optional

--- EleFitsExamples/src/program/EleFitsGenerateAstroObj.cpp
+++ EleFitsExamples/src/program/EleFitsGenerateAstroObj.cpp
@@ -30,6 +30,9 @@ using boost::program_options::value;
 
 using namespace Euclid::Fits;
 
+/*
+ * Some properties of an `AstroObj`.
+ */
 struct AstroObjInfo {
   long combSize;
   long dithCount;
@@ -37,7 +40,17 @@ struct AstroObjInfo {
   Position<2> dith2Shape;
 };
 
-void writeExt(MefFile& f, const std::string& name, const AstroObjInfo& info, long rowCount) {
+void writePrimaryHeader(const Header& h, long nobj) {
+  Record<std::int64_t> nobjRecord {"N_OBJ", nobj, "", "number of objects in the package"};
+  Record<std::string> telescopeRecord {"TELESCOP", "EUCLID", "", "telescope name"};
+  Record<std::string> instrumentRecord {"INSTRUME", "NISP", "", "instrument name"};
+  h.writeSeq(nobjRecord, telescopeRecord, instrumentRecord);
+}
+
+/*
+ * Write a binary table extension which represents one of several `AstroObj`s.
+ */
+const BintableHdu& writeExt(MefFile& f, const std::string& name, const AstroObjInfo& info, long rowCount) {
 
   /* Metadata */
 
@@ -45,15 +58,14 @@ void writeExt(MefFile& f, const std::string& name, const AstroObjInfo& info, lon
   ColumnInfo<std::complex<float>> objRadecInfo("RA_DEC", "deg");
   ColumnInfo<float> expTimeInfo("EXPTIME", "s");
 
-  /* Combined */
+  /* Combined infos */
 
   const auto combSize = info.combSize;
 
   ColumnInfo<float> combSigInfo("COMBINED1D_SIGNAL", "erg", combSize);
-  ColumnInfo<std::int32_t> combQualInfo("COMBINED1D_QUALITY", "", combSize);
   ColumnInfo<float> combVarInfo("COMBINED1D_VAR", "erg^2", combSize);
 
-  /* Dithers */
+  /* Dithers infos */
 
   const auto dithCount = info.dithCount;
   const auto dith1Size = info.dith1Size;
@@ -63,11 +75,9 @@ void writeExt(MefFile& f, const std::string& name, const AstroObjInfo& info, lon
   ColumnInfo<std::int64_t> ptgIdInfo("PTGID", "", dithCount);
 
   ColumnInfo<float, 2> dith1SigInfo("DITH1D_SIGNAL", "erg", {dith1Size, dithCount});
-  ColumnInfo<std::int32_t, 2> dith1QualInfo("DITH1D_QUALITY", "", {dith1Size, dithCount});
   ColumnInfo<float, 2> dith1VarInfo("DITH1D_VAR", "erg^2", {dith1Size, dithCount});
 
   ColumnInfo<float, 3> dith2SigInfo("DITH2D_SIGNAL", "erg", {dith2Width, dith2Height, dithCount});
-  ColumnInfo<std::int32_t, 3> dith2QualInfo("DITH2D_QUALITY", "", {dith2Width, dith2Height, dithCount});
   ColumnInfo<float, 3> dith2VarInfo("DITH2D_VAR", "erg^2", {dith2Width, dith2Height, dithCount});
 
   /* Random data */
@@ -77,41 +87,76 @@ void writeExt(MefFile& f, const std::string& name, const AstroObjInfo& info, lon
   auto expTimeData = Test::generateRandomVector<float>(rowCount);
 
   auto combSigData = Test::generateRandomVector<float>(combSize * rowCount);
-  auto combQualData = Test::generateRandomVector<std::int32_t>(combSize * rowCount);
   auto combVarData = Test::generateRandomVector<float>(combSize * rowCount);
 
   auto ptgIdData = Test::generateRandomVector<std::int64_t>(dithCount * rowCount);
 
   auto dith1SigData = Test::generateRandomVector<float>(dith1Size * dithCount * rowCount);
-  auto dith1QualData = Test::generateRandomVector<std::int32_t>(dith1Size * dithCount * rowCount);
   auto dith1VarData = Test::generateRandomVector<float>(dith1Size * dithCount * rowCount);
 
   auto dith2SigData = Test::generateRandomVector<float>(dith2Width * dith2Height * dithCount * rowCount);
-  auto dith2QualData = Test::generateRandomVector<std::int32_t>(dith2Width * dith2Height * dithCount * rowCount);
   auto dith2VarData = Test::generateRandomVector<float>(dith2Width * dith2Height * dithCount * rowCount);
 
-  /* Write! */
+  /* Create and assign extension */
 
-  f.assignBintableExt(
+  return f.assignBintableExt(
       name,
       makeColumn(std::move(objIdInfo), std::move(objIdData)),
       makeColumn(std::move(objRadecInfo), std::move(objRadecData)),
       makeColumn(std::move(expTimeInfo), std::move(expTimeData)),
       makeColumn(std::move(combSigInfo), std::move(combSigData)),
-      makeColumn(std::move(combQualInfo), std::move(combQualData)),
       makeColumn(std::move(combVarInfo), std::move(combVarData)),
       makeColumn(std::move(ptgIdInfo), std::move(ptgIdData)),
       makeColumn(std::move(dith1SigInfo), std::move(dith1SigData)),
-      makeColumn(std::move(dith1QualInfo), std::move(dith1QualData)),
       makeColumn(std::move(dith1VarInfo), std::move(dith1VarData)),
       makeColumn(std::move(dith2SigInfo), std::move(dith2SigData)),
-      makeColumn(std::move(dith2QualInfo), std::move(dith2QualData)),
       makeColumn(std::move(dith2VarInfo), std::move(dith2VarData)));
 }
 
+/**
+ * Insert quality columns to a binary table HDU.
+ */
+void insertColumns(const BintableColumns& du, const AstroObjInfo& info) {
+
+  /* Infos */
+
+  const auto combSize = info.combSize;
+  const auto dithCount = info.dithCount;
+  const auto dith1Size = info.dith1Size;
+  const auto dith2Width = info.dith2Shape[0];
+  const auto dith2Height = info.dith2Shape[1];
+
+  ColumnInfo<std::int32_t> combQualInfo("COMBINED1D_QUALITY", "", combSize);
+  ColumnInfo<std::int32_t, 2> dith1QualInfo("DITH1D_QUALITY", "", {dith1Size, dithCount});
+  ColumnInfo<std::int32_t, 3> dith2QualInfo("DITH2D_QUALITY", "", {dith2Width, dith2Height, dithCount});
+
+  /* Random data */
+
+  const auto rowCount = du.readRowCount();
+  auto combQualData = Test::generateRandomVector<std::int32_t>(combSize * rowCount);
+  auto dith1QualData = Test::generateRandomVector<std::int32_t>(dith1Size * dithCount * rowCount);
+  auto dith2QualData = Test::generateRandomVector<std::int32_t>(dith2Width * dith2Height * dithCount * rowCount);
+
+  /* Insert before variance columns */
+
+  du.init(combQualInfo, du.readIndex("COMBINED1D_VAR"));
+  du.init(dith1QualInfo, du.readIndex("DITH1D_VAR"));
+  du.init(dith2QualInfo, du.readIndex("DITH2D_VAR"));
+  du.writeSeq(
+      makeColumn(std::move(combQualInfo), std::move(combQualData)),
+      makeColumn(std::move(dith1QualInfo), std::move(dith1QualData)),
+      makeColumn(std::move(dith2QualInfo), std::move(dith2QualData)));
+}
+
+/*
+ * The program.
+ */
 class EleFitsGenerateAstroObj : public Elements::Program {
 
 public:
+  /*
+   * Program options.
+   */
   std::pair<OptionsDescription, PositionalOptionsDescription> defineProgramArguments() override {
     Euclid::Fits::ProgramOptions options("Generate a random AstroObj file, as specified in the SpectrumLib.");
     options.positional("output", value<std::string>()->default_value("/tmp/astroobj.fits"), "Output file");
@@ -120,9 +165,13 @@ public:
     options.named("nbin", value<long>()->default_value(1000), "Wavelength bin count");
     options.named("ndith", value<long>()->default_value(4), "Dither count per AstroObj");
     options.named("height", value<long>()->default_value(15), "Dither 2D height");
+    options.flag("qual", "Flag to write quality columns");
     return options.asPair();
   }
 
+  /*
+   * Run!
+   */
   Elements::ExitCode mainMethod(std::map<std::string, VariableValue>& args) override {
 
     Elements::Logging logger = Elements::Logging::getLogger("EleFitsGenerateAstroObj");
@@ -133,6 +182,7 @@ public:
     const auto nbin = args["nbin"].as<long>();
     const auto ndith = args["ndith"].as<long>();
     const auto height = args["height"].as<long>();
+    const auto qual = args["qual"].as<bool>();
 
     AstroObjInfo info {nbin, ndith, nbin, {nbin, height}};
 
@@ -140,12 +190,14 @@ public:
     MefFile f(filename, FileMode::Overwrite);
 
     logger.info() << "Writing metadata";
-    const auto& primary = f.primary();
-    primary.header().write("N_OBJ", nobj * nhdu);
+    writePrimaryHeader(f.primary().header(), nobj * nhdu);
 
     for (long i = 0; i < nhdu; ++i) {
       logger.info() << "Writing HDU " << i + 1;
-      writeExt(f, std::to_string(i + 1), info, nobj);
+      const auto& ext = writeExt(f, std::to_string(i + 1), info, nobj);
+      if (qual) {
+        insertColumns(ext.columns(), info);
+      }
     }
 
     logger.info() << "Done.";
