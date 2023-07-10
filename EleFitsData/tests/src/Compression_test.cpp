61dfbea5c80357495b4f38357fa8aa3811efc7ea
Antoine Basset <antoine.basser@cnes.fr>
2023-07-10 19:40:28 +0000

    Simplify Compression tests

--- EleFitsData/tests/src/Compression_test.cpp
+++ EleFitsData/tests/src/Compression_test.cpp
@@ -14,7 +14,6 @@ namespace Fits {
 // Dummy definition to please compiler, although the method is not used here
 template <typename TDerived>
 void AlgoMixin<TDerived>::compress(void*) const {}
-// FIXME rm when the function is defined in Compression.hpp
 
 } // namespace Fits
 } // namespace Euclid
@@ -25,38 +24,39 @@ BOOST_AUTO_TEST_SUITE(Compression_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE(factor_test) {
+BOOST_AUTO_TEST_CASE(param_type_test) {
 
-  const float positiveFactor = 5.f;
-  const float zeroFactor = 0.f;
-  const float negativeFactor = -5.f;
+  const double positiveValue = 5;
+  const double zeroValue = 0;
+  const double negativeValue = -5;
 
-  const Param none_f = Param::none();
-  BOOST_TEST(none_f.value() == zeroFactor);
-  BOOST_TEST(none_f.type() == Param::Type::None);
+  const auto none = Param::none();
+  BOOST_TEST(none.value() == zeroValue);
+  BOOST_TEST(none.type() == Param::Type::None);
 
-  const Param absolute_f = Param::absolute(positiveFactor);
-  BOOST_CHECK_THROW(Param::absolute(zeroFactor), Euclid::Fits::FitsError);
-  BOOST_CHECK_THROW(Param::absolute(negativeFactor), Euclid::Fits::FitsError);
+  const auto absolute = Param::absolute(positiveValue);
+  BOOST_CHECK_THROW(Param::absolute(zeroValue), Euclid::Fits::FitsError);
+  BOOST_CHECK_THROW(Param::absolute(negativeValue), Euclid::Fits::FitsError);
 
-  BOOST_TEST(absolute_f.value() == positiveFactor);
-  BOOST_TEST(absolute_f.type() == Param::Type::Absolute);
+  BOOST_TEST(absolute.value() == positiveValue);
+  BOOST_TEST(absolute.type() == Param::Type::Absolute);
 
-  const Param relative_f = Param::relative(positiveFactor);
-  BOOST_CHECK_THROW(Param::relative(zeroFactor), Euclid::Fits::FitsError);
-  BOOST_CHECK_THROW(Param::relative(negativeFactor), Euclid::Fits::FitsError);
+  const auto relative = Param::relative(positiveValue);
+  BOOST_CHECK_THROW(Param::relative(zeroValue), Euclid::Fits::FitsError);
+  BOOST_CHECK_THROW(Param::relative(negativeValue), Euclid::Fits::FitsError);
 
-  BOOST_TEST(relative_f.value() == positiveFactor);
-  BOOST_TEST(relative_f.type() == Param::Type::Relative);
+  BOOST_TEST(relative.value() == positiveValue);
+  BOOST_TEST(relative.type() == Param::Type::Relative);
+}
 
-  // testing == operator:
-  const Param f1 = Param::relative(5.f);
-  const Param f2 = Param::relative(5.f);
-  const Param f3 = Param::absolute(5.f);
-  const Param f4 = Param::relative(4.f);
-  BOOST_TEST((f1 == f2));
-  BOOST_TEST(not(f1 == f3));
-  BOOST_TEST(not(f1 == f4));
+BOOST_AUTO_TEST_CASE(param_equality_test) {
+  const auto r4 = Param::relative(4);
+  const auto r5 = Param::relative(5);
+  const auto r5b = Param::relative(5);
+  const auto a5 = Param::absolute(5);
+  BOOST_TEST((r5 == r5b));
+  BOOST_TEST((r5 != a5));
+  BOOST_TEST((r5 != r4));
 }
 
 BOOST_AUTO_TEST_CASE(default_quantization_test) {
@@ -127,10 +127,7 @@ void testAlgoMixinParameters(long dimension = 0) {
 // specific to the NoCompression algo
 template <>
 void testAlgoMixinParameters<NoCompression>(long) {
-
   NoCompression algo;
-
-  // verify shape of algo is correctly stored at construction
   BOOST_TEST((algo.tiling() == Position<-1>()));
 }
 
@@ -138,39 +135,23 @@ BOOST_AUTO_TEST_CASE(algo_mixin_test) {
 
   testAlgoMixinParameters<NoCompression>();
 
-  testAlgoMixinParameters<Rice>(0);
-  testAlgoMixinParameters<Rice>(1);
-  testAlgoMixinParameters<Rice>(2);
-  testAlgoMixinParameters<Rice>(3);
-  testAlgoMixinParameters<Rice>(4);
-  testAlgoMixinParameters<Rice>(5);
-  testAlgoMixinParameters<Rice>(6);
+  for (long n = 0; n <= 6; ++n) {
+    testAlgoMixinParameters<Rice>(n);
+  }
 
   testAlgoMixinParameters<HCompress>(2);
 
-  testAlgoMixinParameters<Plio>(0);
-  testAlgoMixinParameters<Plio>(1);
-  testAlgoMixinParameters<Plio>(2);
-  testAlgoMixinParameters<Plio>(3);
-  testAlgoMixinParameters<Plio>(4);
-  testAlgoMixinParameters<Plio>(5);
-  testAlgoMixinParameters<Plio>(6);
+  for (long n = 0; n <= 6; ++n) {
+    testAlgoMixinParameters<Plio>(n);
+  }
 
-  testAlgoMixinParameters<Gzip>(0);
-  testAlgoMixinParameters<Gzip>(1);
-  testAlgoMixinParameters<Gzip>(2);
-  testAlgoMixinParameters<Gzip>(3);
-  testAlgoMixinParameters<Gzip>(4);
-  testAlgoMixinParameters<Gzip>(5);
-  testAlgoMixinParameters<Gzip>(6);
+  for (long n = 0; n <= 6; ++n) {
+    testAlgoMixinParameters<Gzip>(n);
+  }
 
-  testAlgoMixinParameters<ShuffledGzip>(0);
-  testAlgoMixinParameters<ShuffledGzip>(1);
-  testAlgoMixinParameters<ShuffledGzip>(2);
-  testAlgoMixinParameters<ShuffledGzip>(3);
-  testAlgoMixinParameters<ShuffledGzip>(4);
-  testAlgoMixinParameters<ShuffledGzip>(5);
-  testAlgoMixinParameters<ShuffledGzip>(6);
+  for (long n = 0; n <= 6; ++n) {
+    testAlgoMixinParameters<ShuffledGzip>(n);
+  }
 }
 
 BOOST_AUTO_TEST_CASE(hcompress_test) {
@@ -183,7 +164,7 @@ BOOST_AUTO_TEST_CASE(hcompress_test) {
   BOOST_TEST(algo.isSmooth() == false);
 
   // setters & getters:
-  Param scale = Param::absolute(5.f);
+  Param scale = Param::absolute(5);
   algo.scale(scale);
   algo.enableSmoothing();
   BOOST_TEST((algo.scale() == scale));
